e73e22457d2458bb589ee3c9136a39aaa43ee184
Davide De Tommaso <davide.detommaso@iit.it>
2020-12-09 14:50:02 +0100

    Changed pip ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import setuptools
 
 setup(name='pyicub',
-      version='1.3',
+      version='1.4',
       description='iCub Python Wrapper',
       url='http://github.com/s4hri/pyicub',
       author='Davide De Tommaso',
