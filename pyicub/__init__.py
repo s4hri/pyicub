367f2a2817acb5fb6b19001cfdd6650299318dd9
Davide De Tommaso <dtmdvd@gmail.com>
2021-07-09 19:02:54 +0200

    Added v3.2

--- pyicub/__init__.py
+++ pyicub/__init__.py
@@ -1,5 +1,5 @@
 __authors__ = 'Davide De Tommaso'
 __emails__ = 'davide.detommaso@iit.it'
 __license__ = 'GPL version 3'
-__version__ = '3.1'
+__version__ = '3.2'
 __description__ = 'Developing iCub applications using Python'
