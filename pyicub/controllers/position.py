6719e3e735de09361e2dfa4bac025e38b8aa0207
Davide De Tommaso <dtmdvd@gmail.com>
2022-03-10 11:15:44 +0100

    Replaced license

--- pyicub/controllers/position.py
+++ pyicub/controllers/position.py
@@ -1,17 +1,30 @@
-#   Copyright (C) 2021  Davide De Tommaso
+# BSD 2-Clause License
 #
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version
+# Copyright (c) 2022, Social Cognition in Human-Robot Interaction,
+#                     Istituto Italiano di Tecnologia, Genova
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+# All rights reserved.
 #
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
 import time
@@ -98,7 +111,7 @@ class PositionController:
     def __setPositionControlMode__(self, joints):
         modes = yarp.IVector(joints, yarp.VOCAB_CM_POSITION)
         self.__IControlMode__.setControlModes(modes)
-        
+
     def getIPositionControl(self):
         return self.__IPositionControl__
 
@@ -115,7 +128,7 @@ class PositionController:
         if timeout == 0.0:
             timeout = PositionController.DEFAULT_TIMEOUT
         self.__logger__.info("""Motion <%d> STARTED!
-                                robot_part:%s, 
+                                robot_part:%s,
                                 target_joints:%s,
                                 req_time:%.2f,
                                 joints_list=%s,
@@ -166,7 +179,7 @@ class PositionController:
             res = self.__waitMotionDone__(timeout=timeout)
             if res:
                 self.__logger__.info("""Motion <%d> COMPLETED!
-                                robot_part:%s, 
+                                robot_part:%s,
                                 target_joints:%s
                                 req_time:%.2f,
                                 joints_list=%s,
@@ -183,7 +196,7 @@ class PositionController:
                                 ))
             else:
                 self.__logger__.warning("""Motion <%d> TIMEOUT!
-                                robot_part:%s, 
+                                robot_part:%s,
                                 target_joints:%s
                                 req_time:%.2f,
                                 joints_list=%s,
