6192c90fd1393ef44c7e363227da112a8b3bc67b
S4HRI <s4hri@iit.it>
2025-05-29 12:22:55 +0200

    Fixed scripts

--- docker/scripts/container/run_apps.sh
+++ docker/scripts/container/run_apps.sh
@@ -6,9 +6,20 @@ initialize_environment
 
 source $(dirname "$0")/setup.sh
 
+if [ "${ICUB_SIMULATION:-true}" != "true" ]; then
+  wait_for_icub_host
+  ensure_ssh_key_installed
+else
+  echo "Running in simulation..."
+fi
+
 terminator 2>/dev/null &
 
 start_local_yarprun
 
 echo "Launching yarpmanager..."
 yarpmanager --apppath "${ICUB_APPS}/applications" --from "${ICUB_APPS}/applications/cluster-config.xml"
+
+if [ "${ICUB_SIMULATION:-true}" != "true" ]; then
+  cleanup_remote_processes
+fi
